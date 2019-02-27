164a7ee01b75064edc50f4382ccce20216686df9
jakobpederson <jakob.pederson@gmail.com>
2019-02-27 11:30:49 -0600

    initial commit

---
+++ install-scripts/OSX/create-symlinks.sh
@@ -0,0 +1,73 @@
+#!/bin/zsh
+#===============================================================================
+#
+#             NOTES: For this to work you must have cloned the github
+#                    repo to your home folder as ~/dotfiles/
+#
+#===============================================================================
+
+#==============
+# Remove old dot flies
+#==============
+sudo rm -rf ~/.vim > /dev/null 2>&1
+sudo rm -rf ~/.vimrc > /dev/null 2>&1
+sudo rm -rf ~/.bashrc > /dev/null 2>&1
+sudo rm -rf ~/.tmux > /dev/null 2>&1
+sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
+sudo rm -rf ~/.zsh_prompt > /dev/null 2>&1
+sudo rm -rf ~/.zshrc > /dev/null 2>&1
+sudo rm -rf ~/.gitconfig > /dev/null 2>&1
+sudo rm -rf ~/.antigen > /dev/null 2>&1
+sudo rm -rf ~/.antigen.zsh > /dev/null 2>&1
+sudo rm -rf ~/.psqlrc > /dev/null 2>&1
+sudo rm -rf ~/.tigrc > /dev/null 2>&1
+sudo rm -rf ~/.config > /dev/null 2>&1
+
+#==============
+# Create symlinks in the home folder
+# Allow overriding with files of matching names in the custom-configs dir
+#==============
+ln -sf ~/dotfiles/vim ~/.vim
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/mac-tmux ~/.tmux
+ln -sf ~/dotfiles/zsh/zsh_prompt ~/.zsh_prompt
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/dotfiles/config ~/.config
+ln -sf ~/dotfiles/custom-configs/custom-snips ~/.vim/custom-snips
+
+if [ -n "$(find ~/dotfiles/custom-configs -name gitconfig)" ]; then
+    ln -s ~/dotfiles/custom-configs/**/gitconfig ~/.gitconfig
+else
+    ln -s ~/dotfiles/gitconfig ~/.gitconfig
+fi
+
+if [ -n "$(find ~/dotfiles/custom-configs -name tmux.conf)" ]; then
+    ln -s ~/dotfiles/custom-configs/**/tmux.conf ~/.tmux.conf
+else
+    ln -s ~/dotfiles/mac-tmux/tmux.conf ~/.tmux.conf
+fi
+
+if [ -n "$(find ~/dotfiles/custom-configs -name tigrc)" ]; then
+    ln -s ~/dotfiles/custom-configs/**/tigrc ~/.tigrc
+else
+    ln -s ~/dotfiles/tigrc ~/.tigrc
+fi
+
+if [ -n "$(find ~/dotfiles/custom-configs -name psqlrc)" ]; then
+    ln -s ~/dotfiles/custom-configs/**/psqlrc ~/.psqlrc
+else
+    ln -s ~/dotfiles/psqlrc ~/.psqlrc
+fi
+
+#==============
+# Set zsh as the default shell
+#==============
+chsh -s /bin/zsh
+
+#==============
+# And we are done
+#==============
+echo -e "\n====== All Done!! ======\n"
+echo
+echo "Enjoy -Jarrod"
---
+++ vim/plugin-configs/syntastic.vim
@@ -0,0 +1,26 @@
+"===============================================================================
+" Plugin source
+"===============================================================================
+"'scrooloose/syntastic'
+
+"===============================================================================
+" Plugin Configurations
+"===============================================================================
+let g:syntastic_check_on_open=1                   " check for errors when file is loaded
+let g:syntastic_loc_list_height=5                 " the height of the error list defaults to 10
+let g:syntastic_python_checkers = ['flake8']      " sets flake8 as the default for checking python files
+let g:syntastic_javascript_checkers = ['eslint']  " sets jshint as our javascript linter
+" Disable syntastic in html files because it pretty well blows there
+let g:syntastic_mode_map={ 'mode': 'active',
+                         \ 'active_filetypes': [],
+                         \ 'passive_filetypes': ['html', 'handlebars'] }
+
+"===============================================================================
+" Plugin Keymappings
+"===============================================================================
+nnoremap <Leader>tl :SyntasticToggleMode<CR>
+
+"===============================================================================
+" Unite Keymap Menu Item(s)
+"===============================================================================
+let g:unite_source_menu_menus.CustomKeyMaps.command_candidates += [['➤ Toggle Linting                                                <Leader>tl', 'SyntasticToggleMode']]
